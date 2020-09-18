738623980c1701a0b474a3d23dc74240ead86078
GaoYangT <2821981852@qq.com>
2020-09-18 19:55:57 +0800

    gaoyang.

--- src/main/java/com/mapper/bm/controller/userController.java
+++ src/main/java/com/mapper/bm/controller/userController.java
@@ -2,12 +2,16 @@ package com.mapper.bm.controller;
 
 import com.mapper.bm.enery.User;
 import com.mapper.bm.service.UserService;
+import com.mapper.bm.util.ResHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class userController {
@@ -17,7 +21,19 @@ public class userController {
     @RequestMapping("/getUserByid")
     @ResponseBody
     public String getUserByid(@RequestParam int id){
+        User user = userService.getUserById(id);
+        List list = ResHandler.ConvertObjToMap(user);
         return userService.getUserById(id).toString();
     }
+
+    @RequestMapping("/selectAll")
+    @ResponseBody
+    public Map<String,Object> selectAll(){
+        List list = userService.selectAll();
+        Map<String,Object> map = new HashMap<>();
+        map.put("resList",list);
+        return map;
+    }
+
 
 }
